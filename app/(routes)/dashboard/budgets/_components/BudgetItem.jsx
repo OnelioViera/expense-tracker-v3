30cda3bd4e3f2e913a600e4ca5d7638503c59534
Onelio Viera <ojvwebdesign@gmail.com>
2024-07-07 17:02:38 -0600

    refactor: Improve BudgetItem component layout and styling

--- app/(routes)/dashboard/budgets/_components/BudgetItem.jsx
+++ app/(routes)/dashboard/budgets/_components/BudgetItem.jsx
@@ -4,17 +4,17 @@ const BudgetItem = ({ budget }) => {
   return (
     <div className='p-5 border rounded-lg'>
       <div className='flex gap-2 items-center justify-between'>
-        <div className='flex gap-2 items-center'>
+        <div className='flex gap-4 items-center'>
           <h2 className='text-2xl p-3 px-3 bg-slate-100 rounded-full'>{budget?.icon}</h2>
           <div>
-            <h2>{budget.name}</h2>
-            <h2>{budget.totalItem} Item</h2>
+            <h2 className='font-semibold text-lg'>{budget.name}</h2>
+            <h2 className=''>{budget.totalItem} Item</h2>
           </div>
         </div>
         <h2 className='font-bold text-primary text-lg'> ${budget.amount}</h2>
       </div>
       <div className='mt-5'>
-        <div className='flex items-center justify-between'>
+        <div className='flex items-center justify-between mb-3'>
           <h2 className='text-sm text-slate-500'>${budget.totalSpend? budget.totalSpend:0} Spent</h2>
           <h2 className='text-sm text-slate-500'>${budget.amount-budget.totalSpend} Remaining</h2>
         </div>
