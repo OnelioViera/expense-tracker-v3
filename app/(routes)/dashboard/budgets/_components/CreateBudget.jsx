468f638239cfd118a7e6bb59aa2e61adb7fcd839
Onelio Viera <ojvwebdesign@gmail.com>
2024-07-07 11:28:20 -0600

    feat: Add emoji picker to CreateBudget dialog

--- app/(routes)/dashboard/budgets/_components/CreateBudget.jsx
+++ app/(routes)/dashboard/budgets/_components/CreateBudget.jsx
@@ -1,12 +1,39 @@
+'use client'
+
 import React from 'react'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import EmojiPicker from 'emoji-picker-react'
+
 
 const CreateBudget = () => {
   return (
     <div>
-      <div className='bg-slate-100 p-10 rounded-md items-center flex flex-col border-2 border-dashed cursor-pointer hover:shadow-md'>
+
+      <Dialog>
+        <DialogTrigger asChild>
+          <div className='bg-slate-100 p-10 rounded-md items-center flex flex-col border-2 border-dashed cursor-pointer hover:shadow-md'>
         <h2 className='text-3xl'>+</h2>
         <h2>Create New Budget</h2>
-      </div>
+      </div></DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Create New Budget</DialogTitle>
+            <DialogDescription>
+              <div>
+                <EmojiPicker />
+              </div>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+
     </div>
   )
 }
