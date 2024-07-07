41815808e95d4aa7045db57cc8aad899b26249a9
Onelio Viera <ojvwebdesign@gmail.com>
2024-07-07 14:40:13 -0600

    refactor: Update BudgetList component to fetch and display budget data

--- app/(routes)/dashboard/budgets/_components/BudgetList.jsx
+++ app/(routes)/dashboard/budgets/_components/BudgetList.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import CreateBudget from './CreateBudget'
+import { db }from '@/utils/dbConfig'
+import { getTableColumns, sql } from 'drizzle-orm'
+import { Budgets } from '@/utils/schema'
 
 const BudgetList = () => {
+
+  const getBudgetList = async () => {
+
+    const result = await db.select({
+      ...getTableColumns(Budgets),
+      totalSpend: sql `sum(${Expenses.amount})`.mapWith(Number),
+    })
+  }
+
   return (
     <div className='mt-7'>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
