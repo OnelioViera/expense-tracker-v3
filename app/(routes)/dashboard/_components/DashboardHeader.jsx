a35e673b750e273db3adef126324479d0985f41a
Onelio Viera <ojvwebdesign@gmail.com>
2024-07-07 08:31:37 -0600

    feat: Update DashboardHeader component to dynamically display title and subtitle

--- app/(routes)/dashboard/_components/DashboardHeader.jsx
+++ app/(routes)/dashboard/_components/DashboardHeader.jsx
@@ -1,12 +1,43 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
 import { UserButton } from '@clerk/nextjs'
-import React from 'react'
+import { usePathname } from 'next/navigation'
 
 const DashboardLayout = () => {
+  const path = usePathname()
+  const [title, setTitle] = useState('Dashboard')
+  const [subtitle, setSubtitle] = useState('')
+
+  useEffect(() => {
+    switch (path) {
+      case '/dashboard':
+        setTitle('Dashboard')
+        setSubtitle('Manage Your Expenses')
+        break
+      case '/dashboard/budgets':
+        setTitle('Budgets')
+        setSubtitle('Create and Manage Budgets')
+        break
+      case '/dashboard/expenses':
+        setTitle('Expenses')
+        setSubtitle('Track Your Expenses')
+        break
+      case '/dashboard/upgrade':
+        setTitle('Upgrade')
+        setSubtitle('Coming Soon')
+        break
+      default:
+        setTitle('Dashboard')
+        setSubtitle('')
+    }
+  }, [path])
+
   return (
-    <div className='p-5 border-b shadow-sm flex justify-between'>
-      <div>
-        
-      </div>
+    <div className='p-5 border-b shadow-sm flex justify-between items-center'>
+      <h1 className='text-2xl'>
+        {title} {subtitle && <span className='text-lg ml-4'>{subtitle}</span>}
+      </h1>
       <div>
         <UserButton />
       </div>
@@ -15,3 +46,4 @@ const DashboardLayout = () => {
 }
 
 export default DashboardLayout
+
